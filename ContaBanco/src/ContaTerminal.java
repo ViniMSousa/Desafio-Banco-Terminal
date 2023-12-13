4776fe7b7b469a68ad15eca4571abbbcd91914b8
ViniMSousa <vini.hmt13@gmail.com>
2023-12-13 02:19:51 -0300

    finalizacao

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
-public class App {
+import java.util.Scanner;
+
+class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Conhecer e importar a classe Scanner
+
+        // Exibir as mensagens para o nosso usuário
+
+        // Obter pela classe Scanner, os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
+
+        int num;
+        String agencia;
+        String nomeCliente;
+        float saldo;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("--- SEJA BEM-VINDO AO SISTEMA DE CRIAÇÃO DE CONTAS DA DIO ---");
+        System.out.println("Por favor, digite o número da agência !");
+        agencia = entrada.nextLine();
+        System.out.println("Por favor, digite seu nome!");
+        nomeCliente = entrada.nextLine();
+        System.out.println("Por favor, digite o número que deseja para sua conta!");
+        num = entrada.nextInt();
+        System.out.println("Por favor, digite o valor de saque inicial para criação da sua conta!");
+        saldo = entrada.nextFloat();
+
+        System.out.println(nomeCliente + ", obrigado por criar uma conta em nosso banco, o número de sua agência é "
+                + agencia
+                + ", seu número de conta é " + num + " e seu saldo é " + saldo
+                + " R$, e já está disponível para saque!");
+
     }
 }
